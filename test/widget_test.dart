072604f5cc1305ffbf32857c100a621ed9573ef5
LionelPerrault <lionelperrault0616@gmail.com>
2022-11-01 13:23:43 +0900

    some minor fixes for project config

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:shnatter/main.dart';
+import 'package:shnatter/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
