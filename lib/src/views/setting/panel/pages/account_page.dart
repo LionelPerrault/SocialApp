3ed342178dc1eabbdfcc41484d0964321d014274
LionelPerrault <lionelperrault0616@gmail.com>
2022-11-29 21:28:41 +0900

    update account setting page

--- lib/src/views/setting/panel/pages/account_page.dart
+++ lib/src/views/setting/panel/pages/account_page.dart
@@ -1,8 +1,11 @@
 
 import 'package:flutter/material.dart';
+import 'package:mvc_pattern/mvc_pattern.dart' as mvc;
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shnatter/src/controllers/UserController.dart';
+import 'package:shnatter/src/managers/user_manager.dart';
 import 'package:shnatter/src/utils/size_config.dart';
 import 'package:shnatter/src/utils/svg.dart';
 import 'package:shnatter/src/views/box/daytimeM.dart';
@@ -12,16 +15,32 @@ import 'package:shnatter/src/views/setting/widget/setting_header.dart';
 import 'package:shnatter/src/widget/mindslice.dart';
 
 class SettingAccountScreen extends StatefulWidget {
-  SettingAccountScreen({Key? key}) : super(key: key);
+  SettingAccountScreen({Key? key}) :
+  con = UserController(), super(key: key);
+  late UserController con;
   @override
   State createState() => SettingAccountScreenState();
 }
 // ignore: must_be_immutable
-class SettingAccountScreenState extends State<SettingAccountScreen> {
+class SettingAccountScreenState extends mvc.StateMVC<SettingAccountScreen> {
   bool showMind = false;
   var user_email = '';
   var user_name = '';
   var password = '';
+  var userInfo = UserManager.userInfo;
+  TextEditingController emailController = TextEditingController();
+  TextEditingController userNameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  late UserController con;
+  @override
+  void initState(){
+    emailController.text = userInfo['email'];
+    userNameController.text = userInfo['userName'];
+    passwordController.text = userInfo['password'];
+    add(widget.con);
+    con = controller as UserController;
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Container(padding: const EdgeInsets.only(top: 20, left:30),
@@ -73,10 +92,7 @@ class SettingAccountScreenState extends State<SettingAccountScreen> {
                             width: 300,
                             height: 30,
                             child: TextFormField(
-                                    onChanged: (newIndex) {
-                                      user_email = newIndex;
-                                      setState(() {});
-                                    },
+                                    controller: emailController,
                                     decoration: InputDecoration(
                                       border: OutlineInputBorder(
                                         borderRadius: BorderRadius.circular(0),
@@ -148,10 +164,7 @@ class SettingAccountScreenState extends State<SettingAccountScreen> {
                         width: 300,
                         height: 30,
                         child: TextFormField(
-                                onChanged: (newIndex) {
-                                  user_email = newIndex;
-                                  setState(() {});
-                                },
+                                controller: userNameController,
                                 decoration: InputDecoration(
                                   border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(0),
@@ -220,10 +233,8 @@ class SettingAccountScreenState extends State<SettingAccountScreen> {
                         width: 350,
                         height: 30,
                         child: TextFormField(
-                                onChanged: (newIndex) {
-                                  user_email = newIndex;
-                                  setState(() {});
-                                },
+                                controller: passwordController,
+                                obscureText: true,
                                 decoration: InputDecoration(
                                   border: OutlineInputBorder(
                                     borderRadius: BorderRadius.circular(0),
@@ -252,7 +263,51 @@ class SettingAccountScreenState extends State<SettingAccountScreen> {
                 const Padding(padding: EdgeInsets.only(top: 20)),
             ]),
           ),
-          SettingFooter()
+          Padding(
+          padding: EdgeInsets.only(right: 20, top: 20),
+          child: Container(
+            height: 65,
+            decoration: const BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: Color.fromARGB(255, 220, 226, 237),
+                  width: 1,
+                )
+              ),
+              color: Color.fromARGB(255, 240, 243, 246),
+              // borderRadius: BorderRadius.all(Radius.circular(3)),
+            ),
+            padding: const EdgeInsets.only(top: 5, left: 15),
+            child: Row(children: [
+              const Flexible(fit: FlexFit.tight, child: SizedBox()),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.all(3),
+                  backgroundColor: Colors.white,
+                  // elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3.0)),
+                  minimumSize: con.isSettingAction ? const Size(90, 50) : const Size(120, 50),
+                  maximumSize: con.isSettingAction ? const Size(90, 50) : const Size(120, 50),
+                ),
+                onPressed: () {
+                  con.saveAccountSettings(emailController.text,userNameController.text,passwordController.text);
+                },
+                child:con.isSettingAction ? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                  SizedBox(
+                    width: 10,
+                    height: 10,
+                    child: CircularProgressIndicator(color: Colors.black),
+                  ),
+                  Padding(padding: EdgeInsets.only(left: 7)),
+                  Text('Loading', style: TextStyle(fontSize: 11, color: Colors.black, fontWeight: FontWeight.bold))
+                ],) : const Text('Save Changes', style: TextStyle(fontSize: 11, color: Colors.black, fontWeight: FontWeight.bold))),
+                const Padding(padding: EdgeInsets.only(right: 30))
+            ],)
+            ),
+        ),
       ],)
       );
   }
