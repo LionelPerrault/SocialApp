bb16f8cb9c8eb5a6db6fe28b5ee0d914c53d4a94
LionelPerrault <lionelperrault0616@gmail.com>
2023-01-05 23:42:55 -0800

    test Ci

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -14,7 +14,7 @@ import 'package:flutter/foundation.dart'
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
 /// ```
-class DefaultFirebaseOptions {
+class DefaultFirebaseOptions { 
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
       return web;
