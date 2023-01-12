99ae0983df4f0bccb9cda3109da6ef29fee7171e
LionelPerrault <lionelperrault0616@gmail.com>
2023-01-12 16:36:35 +0100

    user model update

--- lib/src/models/userModel.dart
+++ lib/src/models/userModel.dart
@@ -6,14 +6,12 @@ class TokenLogin {
   late String birthM = '';
   late String u_class = '';
   late String country = '';
-  late String relysiaEmail = '';
   late String firstName = '';
   late String lastName = '';
   late String password = '';
   late String paymail = '';
   late String walletAddress = '';
   late String documentId = '';
-  late String relysiaPassword = '';
   late String userName = '';
   late String userAvatar = '';
   late bool isStarted = false;
@@ -27,8 +25,6 @@ class TokenLogin {
       firstName = json['firstName'] as String;
       lastName = json['lastName'] as String;
       userName = json['userName'] as String;
-      relysiaEmail = json['relysiaEmail'] as String;
-      relysiaPassword = json['relysiaPassword'] as String;
       paymail = json['paymail'] as String;
       walletAddress = json['walletAddress'] as String;
       isStarted = json['isStarted'] as bool;
@@ -46,8 +42,6 @@ class TokenLogin {
       'lastName': lastName,
       'userName': userName,
       'password': password,
-      'relysiaEmail': relysiaEmail,
-      'relysiaPassword': relysiaPassword,
       'walletAddress': walletAddress,
       'paymail': paymail,
       'isStarted': isStarted,
