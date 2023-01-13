7f717e3251b02c5b7e9ca7f5ddd0c4633415c07f
LionelPerrault <lionelperrault0616@gmail.com>
2023-01-13 14:23:39 -0500

    update email verification function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-const firebase = require("firebase");
 const axios = require('axios');
 const cors = require('cors')({origin: true});
 admin.initializeApp();
@@ -10,18 +9,19 @@ exports.offlineRequest = functions.https.onRequest(async (req,res) => {
     res.set("Access-Control-Allow-Origin", "*"); // you can also whitelist a specific domain like "http://127.0.0.1:4000"
     res.set("Access-Control-Allow-Headers", "Content-Type");
     var userName = req.body.userName
-    var snapshot = await firebase.firestore().collection('onlineStatus').where('userName','==',userName).get()
+    var snapshot = await admin.firestore().collection('onlineStatus').where('userName','==',userName).get()
     if(snapshot.docs.length == 0){
-      await firebase.firestore().collection('onlineStatus').add({
+      await admin.firestore().collection('onlineStatus').add({
         'userName':userName,
         'status':0
       })
     }
     else{
-      await firebase.firestore().collection('onlineStatus').doc(snapshot.docs[0].id).update({
+      await admin.firestore().collection('onlineStatus').doc(snapshot.docs[0].id).update({
         'status':0
       })
     }
+    console.log('asdf', 'asdfasdfwer234235');
     res.send('ok')
   })
 })
@@ -38,54 +38,62 @@ exports.emailVerification = functions.https.onRequest(async (req, res) => {
     const adminPassword = 'entertainment';
     const adminPaymail = '4326@shnatter.com';
     const uuid = req.query.uid;
-    const snapshot = await admin.firestore().collection('user').doc(uuid).get();
+    const snapshot = await admin.firestore().collection('user').doc(`${uuid}`).get();
     const payMail = snapshot.data().paymail;
     var token;
     var balance = 10;
-
     var notes = 'Thank you for creating your account in our website !';
     var bodyCode = {'email': adminEmail, 'password': adminPassword};
-    await axios({
-      method: 'post', //you can set what request you want to be
-      url: apiUrlAuth,
-      data: bodyCode,
-      headers: {
-        'content-type': 'application/json',
-        'serviceID': serviceId
-      }
-    })
-    .then((res) => {
-      console.log('res.body: ', res['data']['data']['token']);
-      token = res['data']['data']['token'];
-    });
-    await axios({
-      method: 'post', //you can set what request you want to be
-      url: 'https://api.relysia.com/v1/send',
-      data: { "dataArray" : [
+
+    if(!snapshot.data().isEmailVerify){
+      await axios({
+        method: 'post', //you can set what request you want to be
+        url: apiUrlAuth,
+        data: bodyCode,
+        headers: {
+          'content-type': 'application/json',
+          'serviceID': serviceId
+        }
+      })
+      .then((res) => {
+        console.log('res.body: ', res['data']['data']['token']);
+        token = res['data']['data']['token'];
+      });
+      await axios({
+        method: 'post', //you can set what request you want to be
+        url: 'https://api.relysia.com/v1/send',
+        data: { "dataArray" : [
+          {
+            'to' : payMail, 
+            'amount' : balance, 
+            'tokenId' : shnToken, 
+            'notes': notes
+          }
+        ]},
+        headers: {
+          'authToken': token,
+          'content-type': 'application/json',
+          'serviceID': serviceId,
+        }
+      })
+      .then(
+        (res) => {
+          if (res['data']['statusCode'] == 200){
+            // returnData = 'Successfully paid',
+            console.log("success" + res['data']['statusCode']);
+          }else {
+            // returnData = 'Failed payment';
+            console.log('Failed payment: ', 'Failed payment');
+          }
+        },
+      );
+      admin.firestore().collection('user').doc(`${uuid}`).set(
         {
-          'to' : payMail, 
-          'amount' : balance, 
-          'tokenId' : shnToken, 
-          'notes': notes
-        }
-      ]},
-      headers: {
-        'authToken': token,
-        'content-type': 'application/json',
-        'serviceID': serviceId,
-      }
-    })
-    .then(
-      (res) => {
-        if (res['data']['statusCode'] == 200){
-          // returnData = 'Successfully paid',
-          console.log("success" + res['data']['statusCode']);
-        }else {
-          // returnData = 'Failed payment';
-          console.log('Failed payment: ', 'Failed payment');
-        }
-      },
-    );
+          isEmailVerify: true
+        },
+        { merge: true },
+      );
+    }
     return res.redirect('https://shnatter-a69cd.web.app/');
   })
 })
