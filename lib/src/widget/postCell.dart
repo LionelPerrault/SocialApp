7dd928afeb1a0bdc9a7a25ff5a249b0887efb8c8
LionelPerrault <lionelperrault0616@gmail.com>
2023-01-25 12:32:01 +0100

    upload photo and feeling activity

--- lib/src/widget/postCell.dart
+++ lib/src/widget/postCell.dart
@@ -46,7 +46,11 @@ class PostCellState extends mvc.StateMVC<PostCell> {
 
   @override
   Widget build(BuildContext context) {
-    return picturePostCell();
+    return widget.postInfo['type'] == 'photo'
+        ? picturePostCell()
+        : widget.postInfo['type'] == 'photo'
+            ? feelingPostCell()
+            : SizedBox();
   }
 
   Widget picturePostCell() {
@@ -155,16 +159,24 @@ class PostCellState extends mvc.StateMVC<PostCell> {
                           )
                         ],
                       ),
-                      const Padding(padding: EdgeInsets.only(top: 15)),
-                      // Row(
-                      //   children: widget.postInfo['data']
-                      //       .map((picture) => photo(picture['url']))
-                      //       .toList(),
-                      // )
+                      const Padding(padding: EdgeInsets.only(top: 30)),
+                      Row(
+                        children: [
+                          for (var item in widget.postInfo['data'])
+                            Expanded(
+                              child: Container(
+                                height: 250,
+                                child: Image.network(item['url'],
+                                    fit: BoxFit.cover),
+                              ),
+                            )
+                        ],
+                      )
                     ],
                   ),
                 ),
-                // LikesCommentScreen(productId: widget.postInfo['id'])
+                const Padding(padding: EdgeInsets.only(top: 30)),
+                LikesCommentScreen(productId: widget.postInfo['id'])
               ],
             ),
           ),
@@ -173,12 +185,135 @@ class PostCellState extends mvc.StateMVC<PostCell> {
     );
   }
 
-  Widget photo(url) {
-    return Expanded(
-      child: Container(
-        height: 200,
-        child: Image.network(url, fit: BoxFit.cover),
-      ),
+  Widget feelingPostCell() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            margin: const EdgeInsets.only(top: 30, bottom: 30),
+            width: 600,
+            padding: const EdgeInsets.only(top: 20),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 22,
+                            child: SvgPicture.network(Helper.avatar),
+                          ),
+                          const Padding(padding: EdgeInsets.only(left: 10)),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                        style: const TextStyle(
+                                            color: Colors.grey, fontSize: 10),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                              text:
+                                                  '${widget.postInfo['admin']['firstName']} ${widget.postInfo['admin']['lastName']}',
+                                              style: const TextStyle(
+                                                  color: Colors.black,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 16),
+                                              recognizer: TapGestureRecognizer()
+                                                ..onTap = () {
+                                                  Navigator.pushReplacementNamed(
+                                                      context,
+                                                      '/${widget.postInfo['admin']['userName']}');
+                                                })
+                                        ]),
+                                  ),
+                                  Container(
+                                    width: SizeConfig(context).screenWidth < 600
+                                        ? SizeConfig(context).screenWidth - 240
+                                        : 350,
+                                    child: Text(
+                                      ' added ${widget.postInfo['data'].length == 1 ? 'a' : widget.postInfo['data'].length} photo${widget.postInfo['data'].length == 1 ? '' : 's'}',
+                                      style: const TextStyle(fontSize: 14),
+                                    ),
+                                  ),
+                                  // Container(
+                                  //   padding: EdgeInsets.only(right: 9.0),
+                                  //   child: CustomPopupMenu(
+                                  //       menuBuilder: () => SubFunction(),
+                                  //       pressType: PressType.singleClick,
+                                  //       verticalMargin: -10,
+                                  //       child:
+                                  //           const Icon(Icons.arrow_drop_down)),
+                                  // ),
+                                ],
+                              ),
+                              const Padding(padding: EdgeInsets.only(top: 3)),
+                              Row(
+                                children: [
+                                  RichText(
+                                    text: TextSpan(
+                                        style: const TextStyle(
+                                            color: Colors.grey, fontSize: 10),
+                                        children: <TextSpan>[
+                                          TextSpan(
+                                              // text: Helper.formatDate(
+                                              //     widget.postInfo['time']),
+                                              text: '',
+                                              style: const TextStyle(
+                                                  color: Colors.black,
+                                                  fontSize: 10),
+                                              recognizer: TapGestureRecognizer()
+                                                ..onTap = () {
+                                                  Navigator.pushReplacementNamed(
+                                                      context,
+                                                      '${RouteNames.products}/${widget.postInfo['id']}');
+                                                })
+                                        ]),
+                                  ),
+                                  const Text(' - '),
+                                  const Icon(
+                                    Icons.language,
+                                    size: 12,
+                                  )
+                                ],
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      const Padding(padding: EdgeInsets.only(top: 30)),
+                      Row(
+                        children: [
+                          for (var item in widget.postInfo['data'])
+                            Expanded(
+                              child: Container(
+                                height: 250,
+                                child: Image.network(item['url'],
+                                    fit: BoxFit.cover),
+                              ),
+                            )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.only(top: 30)),
+                LikesCommentScreen(productId: widget.postInfo['id'])
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 
