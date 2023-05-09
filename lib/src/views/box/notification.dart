7afad2d4d81fc74b31591d9015e82fa10c874d92
LionelPerrault <lionelperrault0616@gmail.com>
2023-05-09 00:50:06 -0700

    remove notification sound

--- lib/src/views/box/notification.dart
+++ lib/src/views/box/notification.dart
@@ -59,37 +59,37 @@ class ShnatterNotificationState extends mvc.StateMVC<ShnatterNotification> {
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Padding(
+                children: const [
+                  Padding(
                       padding: EdgeInsets.only(left: 10),
                       child: Text(
                         "Notifications",
                         style: TextStyle(
                             fontSize: 15, fontWeight: FontWeight.bold),
                       )),
-                  Row(children: [
-                    const Text(
-                      'Alert Sound',
-                      style: TextStyle(fontSize: 11),
-                    ),
-                    SizedBox(
-                      height: 20,
-                      child: Transform.scale(
-                        scaleX: 0.55,
-                        scaleY: 0.55,
-                        child: CupertinoSwitch(
-                          //thumbColor: kprimaryColor,
-                          activeColor: kprimaryColor,
-                          value: isSound,
-                          onChanged: (value) {
-                            setState(() {
-                              isSound = value;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ])
+                  // Row(children: [
+                  //   const Text(
+                  //     'Alert Sound',
+                  //     style: TextStyle(fontSize: 11),
+                  //   ),
+                  //   SizedBox(
+                  //     height: 20,
+                  //     child: Transform.scale(
+                  //       scaleX: 0.55,
+                  //       scaleY: 0.55,
+                  //       child: CupertinoSwitch(
+                  //         //thumbColor: kprimaryColor,
+                  //         activeColor: kprimaryColor,
+                  //         value: isSound,
+                  //         onChanged: (value) {
+                  //           setState(() {
+                  //             isSound = value;
+                  //           });
+                  //         },
+                  //       ),
+                  //     ),
+                  //   ),
+                  // ])
                 ],
               ),
               const SizedBox(
