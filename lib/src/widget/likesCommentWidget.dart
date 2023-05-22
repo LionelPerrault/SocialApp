ab20751e7823747cf9183bb0b7150874f7dea6a7
LionelPerrault <lionelperrault0616@gmail.com>
2023-05-21 22:34:47 -0700

    write commect grey screen

--- lib/src/widget/likesCommentWidget.dart
+++ lib/src/widget/likesCommentWidget.dart
@@ -1120,74 +1120,78 @@ class LikesCommentScreenState extends mvc.StateMVC<LikesCommentScreen> {
                       margin: const EdgeInsets.only(top: 10),
                       child: Column(
                         children: [
-                          inputReply((value) {
-                            reply[e['id']] = value;
-                          }, () async {
-                            print("reply[e['id']]  is ${reply[e['id']]}");
-                            if (reply[e['id']] != null &&
-                                reply[e['id']] != '') {
+                          inputReply(
+                            (value) {
+                              reply[e['id']] = value;
+                            },
+                            () async {
                               print("reply[e['id']]  is ${reply[e['id']]}");
-                              await con.saveReply(widget.postInfo['id'],
-                                  e['id'], reply[e['id']], 'text');
-                              reply[e['id']] = '';
-                              //   whatReply.remove(e['id']);
-                              setState(() {});
-                            }
-                          }, () {
-                            setState(() {
-                              emojiShowingOfReply = !emojiShowingOfReply;
-                            });
-                          }),
-                          Offstage(
-                            offstage: !emojiShowingOfReply,
-                            child: Expanded(
-                              child: SizedBox(
-                                height: 250,
-                                child: EmojiPicker(
-                                  onEmojiSelected: (category, Emoji emoji) {
-                                    comment = comment + emoji.emoji;
-                                  },
-                                  textEditingController: commentController,
-                                  config: Config(
-                                    columns: 7,
-                                    // Issue: https://github.com/flutter/flutter/issues/28894
-                                    emojiSizeMax: 32 *
-                                        (foundation.defaultTargetPlatform ==
-                                                TargetPlatform.iOS
-                                            ? 1.30
-                                            : 1.0),
-                                    verticalSpacing: 0,
-                                    horizontalSpacing: 0,
-                                    gridPadding: EdgeInsets.zero,
-                                    initCategory: Category.RECENT,
-                                    bgColor: const Color(0xFFF2F2F2),
-                                    indicatorColor: Colors.blue,
-                                    iconColor: Colors.grey,
-                                    iconColorSelected: Colors.blue,
-                                    backspaceColor: Colors.blue,
-                                    skinToneDialogBgColor: Colors.white,
-                                    skinToneIndicatorColor: Colors.grey,
-                                    enableSkinTones: true,
-                                    showRecentsTab: true,
-                                    recentsLimit: 28,
-                                    replaceEmojiOnLimitExceed: false,
-                                    noRecents: const Text(
-                                      'No Recents',
-                                      style: TextStyle(
-                                          fontSize: 20, color: Colors.black26),
-                                      textAlign: TextAlign.center,
-                                    ),
-                                    loadingIndicator: const SizedBox.shrink(),
-                                    tabIndicatorAnimDuration:
-                                        kTabScrollDuration,
-                                    categoryIcons: const CategoryIcons(),
-                                    buttonMode: ButtonMode.MATERIAL,
-                                    checkPlatformCompatibility: true,
-                                  ),
-                                ),
-                              ),
-                            ),
+                              if (reply[e['id']] != null &&
+                                  reply[e['id']] != '') {
+                                print("reply[e['id']]  is ${reply[e['id']]}");
+                                await con.saveReply(widget.postInfo['id'],
+                                    e['id'], reply[e['id']], 'text');
+                                reply[e['id']] = '';
+                                //   whatReply.remove(e['id']);
+                                setState(() {});
+                              }
+                            },
+                            () {
+                              setState(() {
+                                emojiShowingOfReply = !emojiShowingOfReply;
+                              });
+                            },
                           ),
+                          // Offstage(
+                          //   offstage: !emojiShowingOfReply,
+                          //   child: Expanded(
+                          //     child: SizedBox(
+                          //       height: 250,
+                          //       child: EmojiPicker(
+                          //         onEmojiSelected: (category, Emoji emoji) {
+                          //           comment = comment + emoji.emoji;
+                          //         },
+                          //         textEditingController: commentController,
+                          //         config: Config(
+                          //           columns: 7,
+                          //           // Issue: https://github.com/flutter/flutter/issues/28894
+                          //           emojiSizeMax: 32 *
+                          //               (foundation.defaultTargetPlatform ==
+                          //                       TargetPlatform.iOS
+                          //                   ? 1.30
+                          //                   : 1.0),
+                          //           verticalSpacing: 0,
+                          //           horizontalSpacing: 0,
+                          //           gridPadding: EdgeInsets.zero,
+                          //           initCategory: Category.RECENT,
+                          //           bgColor: const Color(0xFFF2F2F2),
+                          //           indicatorColor: Colors.blue,
+                          //           iconColor: Colors.grey,
+                          //           iconColorSelected: Colors.blue,
+                          //           backspaceColor: Colors.blue,
+                          //           skinToneDialogBgColor: Colors.white,
+                          //           skinToneIndicatorColor: Colors.grey,
+                          //           enableSkinTones: true,
+                          //           showRecentsTab: true,
+                          //           recentsLimit: 28,
+                          //           replaceEmojiOnLimitExceed: false,
+                          //           noRecents: const Text(
+                          //             'No Recents',
+                          //             style: TextStyle(
+                          //                 fontSize: 20, color: Colors.black26),
+                          //             textAlign: TextAlign.center,
+                          //           ),
+                          //           loadingIndicator: const SizedBox.shrink(),
+                          //           tabIndicatorAnimDuration:
+                          //               kTabScrollDuration,
+                          //           categoryIcons: const CategoryIcons(),
+                          //           buttonMode: ButtonMode.MATERIAL,
+                          //           checkPlatformCompatibility: true,
+                          //         ),
+                          //       ),
+                          //     ),
+                          //   ),
+                          // ),
                         ],
                       ),
                     )
