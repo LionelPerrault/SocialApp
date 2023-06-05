adced1521e359e22dfd1531f92dabb4ce929a136
LionelPerrault <lionelperrault0616@gmail.com>
2023-06-05 08:26:45 -0400

    428

--- lib/src/views/people/peoplescreen.dart
+++ lib/src/views/people/peoplescreen.dart
@@ -29,7 +29,6 @@ class PeopleScreen extends StatefulWidget {
 class PeopleScreenState extends mvc.StateMVC<PeopleScreen>
     with SingleTickerProviderStateMixin {
   late PeopleController con;
-  final ScrollController _scrollController = ScrollController();
   //route variable
   @override
   void initState() {
@@ -66,177 +65,184 @@ class PeopleScreenState extends mvc.StateMVC<PeopleScreen>
   }
 
   Widget mainTabWidget() {
-    return SingleChildScrollView(
+    return Container(
+      alignment: Alignment.center,
+      width: SizeConfig(context).screenWidth > SizeConfig.mediumScreenSize
+          ? SizeConfig(context).screenWidth - SizeConfig.leftBarWidth
+          : SizeConfig(context).screenWidth,
+      child: SingleChildScrollView(
         scrollDirection: Axis.horizontal,
-        controller: _scrollController,
         child: Container(
           height: 67,
-          width: SizeConfig(context).screenWidth < 900
-              ? SizeConfig(context).screenWidth
-              : SizeConfig(context).screenWidth * 0.7,
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(3),
           ),
           child: Row(children: [
             InkWell(
-                onTap: () async {
-                  con.tabName = 'Discover';
-                  // reload again.
+              onTap: () async {
+                con.tabName = 'Discover';
+                // reload again.
 
-                  setState(() {});
-                },
-                child: Container(
-                  padding: EdgeInsets.only(top: 19.5),
-                  width: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Discover',
-                        style: TextStyle(color: Colors.grey, fontSize: 14),
-                      ),
-                      con.tabName == 'Discover'
-                          ? Container(
-                              margin: EdgeInsets.only(top: 4.5),
-                              height: 2,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(2))),
-                            )
-                          : Container(
-                              margin: EdgeInsets.only(top: 19),
-                            )
-                    ],
-                  ),
-                )),
+                setState(() {});
+              },
+              child: Container(
+                padding: EdgeInsets.only(top: 19.5),
+                width: 100,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Discover',
+                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                    con.tabName == 'Discover'
+                        ? Container(
+                            margin: EdgeInsets.only(top: 4.5),
+                            height: 2,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2))),
+                          )
+                        : Container(
+                            margin: EdgeInsets.only(top: 19),
+                          )
+                  ],
+                ),
+              ),
+            ),
             InkWell(
-                onTap: () async {
-                  con.tabName = 'Friends';
-                  // reload again.
+              onTap: () async {
+                con.tabName = 'Friends';
+                // reload again.
 
-                  setState(() {});
-                },
-                child: Container(
-                  padding: EdgeInsets.only(top: 19.5),
-                  width: 100,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Friends',
-                        style: TextStyle(color: Colors.grey, fontSize: 14),
-                      ),
-                      con.tabName == 'Friends'
-                          ? Container(
-                              margin: EdgeInsets.only(top: 4.5),
-                              height: 2,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(2))),
-                            )
-                          : Container(
-                              margin: EdgeInsets.only(top: 19),
-                            )
-                    ],
-                  ),
-                )),
+                setState(() {});
+              },
+              child: Container(
+                padding: EdgeInsets.only(top: 19.5),
+                width: 100,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Friends',
+                      style: TextStyle(color: Colors.grey, fontSize: 14),
+                    ),
+                    con.tabName == 'Friends'
+                        ? Container(
+                            margin: EdgeInsets.only(top: 4.5),
+                            height: 2,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2))),
+                          )
+                        : Container(
+                            margin: EdgeInsets.only(top: 19),
+                          )
+                  ],
+                ),
+              ),
+            ),
             InkWell(
-                onTap: () async {
-                  con.tabName = 'Friend Requests';
-                  setState(() {});
-                },
-                child: Container(
-                  padding: EdgeInsets.only(top: 15),
-                  width: 150,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Friend Requests',
-                            style: TextStyle(color: Colors.grey, fontSize: 14),
+              onTap: () async {
+                con.tabName = 'Friend Requests';
+                setState(() {});
+              },
+              child: Container(
+                padding: EdgeInsets.only(top: 15),
+                width: 150,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Friend Requests',
+                          style: TextStyle(color: Colors.grey, fontSize: 14),
+                        ),
+                        Padding(padding: EdgeInsets.only(left: 10)),
+                        badges.Badge(
+                          badgeStyle: badges.BadgeStyle(
+                            badgeColor: Colors.blue,
                           ),
-                          Padding(padding: EdgeInsets.only(left: 10)),
-                          badges.Badge(
-                            badgeStyle: badges.BadgeStyle(
-                              badgeColor: Colors.blue,
-                            ),
-                            badgeContent: Text(
-                              con.requestFriends.length.toString(),
+                          badgeContent: Text(
+                            con.requestFriends.length.toString(),
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        )
+                      ],
+                    ),
+                    con.tabName == 'Friend Requests'
+                        ? Container(
+                            //margin: EdgeInsets.only(top: 17),
+                            height: 2,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2))),
+                          )
+                        : Container(
+                            margin: EdgeInsets.only(top: 19),
+                          )
+                  ],
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () async {
+                con.tabName = 'Requests Sent';
+                setState(() {});
+              },
+              child: Container(
+                width: 130,
+                padding: EdgeInsets.only(top: 15),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Requests Sent',
+                          style: TextStyle(color: Colors.grey, fontSize: 14),
+                        ),
+                        Padding(padding: EdgeInsets.only(left: 10)),
+                        badges.Badge(
+                          badgeStyle: badges.BadgeStyle(
+                            badgeColor: Colors.blue,
+                          ),
+                          badgeContent: ChangeNotifierProvider(
+                            create: (context) => con.sendBadge,
+                            child: Text(
+                              con.sendBadge.badgeNumber.toString(),
                               style: TextStyle(color: Colors.white),
                             ),
-                          )
-                        ],
-                      ),
-                      con.tabName == 'Friend Requests'
-                          ? Container(
-                              //margin: EdgeInsets.only(top: 17),
-                              height: 2,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(2))),
-                            )
-                          : Container(
-                              margin: EdgeInsets.only(top: 19),
-                            )
-                    ],
-                  ),
-                )),
-            InkWell(
-                onTap: () async {
-                  con.tabName = 'Requests Sent';
-                  setState(() {});
-                },
-                child: Container(
-                  width: 130,
-                  padding: EdgeInsets.only(top: 15),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Requests Sent',
-                            style: TextStyle(color: Colors.grey, fontSize: 14),
                           ),
-                          Padding(padding: EdgeInsets.only(left: 10)),
-                          badges.Badge(
-                            badgeStyle: badges.BadgeStyle(
-                              badgeColor: Colors.blue,
-                            ),
-                            badgeContent: ChangeNotifierProvider(
-                              create: (context) => con.sendBadge,
-                              child: Text(
-                                con.sendBadge.badgeNumber.toString(),
-                                style: TextStyle(color: Colors.white),
-                              ),
-                            ),
+                        )
+                      ],
+                    ),
+                    con.tabName == 'Requests Sent'
+                        ? Container(
+                            //margin: EdgeInsets.only(top: 17),
+                            height: 2,
+                            decoration: BoxDecoration(
+                                color: Colors.black,
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(2))),
                           )
-                        ],
-                      ),
-                      con.tabName == 'Requests Sent'
-                          ? Container(
-                              //margin: EdgeInsets.only(top: 17),
-                              height: 2,
-                              decoration: BoxDecoration(
-                                  color: Colors.black,
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(2))),
-                            )
-                          : Container(
-                              margin: EdgeInsets.only(top: 19),
-                            )
-                    ],
-                  ),
-                ))
+                        : Container(
+                            margin: EdgeInsets.only(top: 19),
+                          )
+                  ],
+                ),
+              ),
+            )
           ]),
-        ));
+        ),
+      ),
+    );
   }
 }
